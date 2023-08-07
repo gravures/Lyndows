4ea63233af166cb9290a285bf344cc12c0c3dbed
Gilles Coissac <gilles@ideographe.fr>
2023-08-07 08:30:51 +0200

    Simplify UPath hierarchy removing PurePath versions, only use concret path now.

--- src/lyndows/path.py
+++ src/lyndows/path.py
@@ -25,6 +25,7 @@ from functools import wraps
 from pathlib import (
     Path,
     PurePath,
+    PurePosixPath,
     PureWindowsPath,
     WindowsPath,
     _posix_flavour,  # type: ignore
@@ -58,17 +59,24 @@ def is_windows_path(path: FilePath) -> bool:
     return isinstance(path, (PureWindowsPath, WindowsPath)) or split_drive(path)[0] != ""
 
 
+class _WineFlavour(_WindowsFlavour):
+    is_supported = not on_windows()
+
+
+_wine_flavour = _WineFlavour()
+
+
 class UMeta(type):
     def __new__(
         cls, name: str, bases: tuple[type, ...], namespace: dict[str, Any], **kwargs: Any
     ) -> UMeta:
         if name == "UPosixPath":
-            bases = (UPath, Path, UPurePosixPath)
+            bases = (UPath, Path, PurePosixPath)
         elif name == "UWindowsPath":
-            bases = (UPath, Path, UPureWindowsPath)
-            namespace = cls.__prepare_api__(
-                (PurePath, UPurePath, UPureWindowsPath, UPath), namespace
-            )
+            bases = (UPath, Path, PureWindowsPath)
+        elif name == "UWinePath":
+            bases = (UPath, Path, PureWindowsPath)
+            namespace = cls.__prepare_api__((PurePath, UPath), namespace)
         return type.__new__(cls, name, bases, namespace, **kwargs)
 
     def __init__(
@@ -104,31 +112,17 @@ class UMeta(type):
         return super().__prepare__(name, bases, **kwargs)
 
     def __call__(self, *args: Any, **kwds: Any) -> Any:
-        if self.callby == "UPurePath":
+        if self.callby == "UPath":
             if on_windows():
-                return UPureWindowsPath(*args, **kwds)
-            elif args and is_windows_path(args[0]):
-                return UPureWindowsPath(*args, **kwds)
-            else:
-                return UPurePosixPath(*args, **kwds)
-        elif self.callby == "UPath":
-            if on_windows():
-                return Path(*args, **kwds)
-            elif args and is_windows_path(args[0]):
                 return UWindowsPath(*args, **kwds)
+            elif args and is_windows_path(args[0]):
+                return UWinePath(*args, **kwds)
             else:
                 return UPosixPath(*args, **kwds)
         return super().__call__(*args, **kwds)
 
 
-class _WineFlavour(_WindowsFlavour):
-    is_supported = not on_windows()
-
-
-_wine_flavour = _WineFlavour()
-
-
-class UPurePath(PurePath, metaclass=UMeta):
+class UPath(Path, metaclass=UMeta):
     """PurePath offering support for Wine filesystem.
 
     Extends PurePath standard library and represents a filesystem
@@ -144,6 +138,15 @@ class UPurePath(PurePath, metaclass=UMeta):
     _sys_mount_points = {
         part.mountpoint: part.device for part in psutil.disk_partitions()
     }
+
+    @classmethod
+    def _from_parts(cls, args):
+        self = super()._from_parts(args)  # type: ignore
+        if not self._flavour.is_supported:
+            raise NotImplementedError(
+                "cannot instantiate %r on your system" % (cls.__name__,)
+            )
+        return self
 
     def _new_winepfx(self) -> None:
         self._winepfx: Prefix | None = None
@@ -173,97 +176,20 @@ class UPurePath(PurePath, metaclass=UMeta):
                 if mnt in self._sys_mount_points:
                     self._mount_points[mnt] = dev.name
 
-    def expanduser(self) -> UPurePath:
-        # TODO: './~/case'
-        if self.parts and self.parts[0] == "~":
-            return self.__class__.__new__(self.__class__, Path.home(), *self.parts[1:])
-        return self
-
-    def absolute(self) -> UPurePath:
-        return (
-            self
-            if self.is_absolute()
-            else self.__class__.__new__(self.__class__, Path.cwd(), self)
-        )
-
-    def is_mount(self) -> bool:
-        return str(self.expanduser()) in self._sys_mount_points
-
-    def mount_point(self) -> UPurePath:
+    def mount_point(self) -> UPath:
         path = self.expanduser().absolute()
         while not path.is_mount():
             path = path.parent
         return path
 
-    def as_native(self) -> UPurePath | PurePath:
+    def as_native(self) -> UPath:
         return self
 
-    def as_windows(self) -> UPurePath | PurePath:
+    def as_windows(self) -> UPath:
         return self
 
 
-UFilePath = Union[str, UPurePath]
-
-
-class UPurePosixPath(UPurePath):
-    __slots__ = ()
-    _flavour = _posix_flavour
-
-    def __new__(cls, *args: FilePath) -> UPurePosixPath:
-        self: UPurePosixPath = UPurePosixPath._from_parts(args)  # type: ignore
-        self._new_winepfx()
-        return self
-
-    def _map_parts(self) -> tuple[str, UPurePosixPath]:
-        mnt = self.mount_point()
-        drive = self._mount_points.get(str(mnt), "")
-        path = self.expanduser().absolute()
-        path = path.relative_to(mnt) if drive else path
-        drive = drive or self._mount_points.get("/", "")
-        return drive, path  # type: ignore
-
-    def as_windows(self) -> UPurePath:
-        if on_windows():
-            return UPureWindowsPath(self)
-        drive, path = self._map_parts()
-        return UPureWindowsPath(f"{drive}/{path}")
-
-    def as_native(self) -> UPurePath:
-        return self.as_windows() if on_windows() else self
-
-
-class UPureWindowsPath(UPurePath):
-    __slots__ = ()
-    _flavour = _windows_flavour
-
-    def __new__(cls, *args: FilePath) -> UPureWindowsPath:
-        self: UPureWindowsPath = UPureWindowsPath._from_parts(args)  # type: ignore
-        self._new_winepfx()
-        return self
-
-    def absolute(self) -> UPureWindowsPath:
-        if self.is_absolute():
-            return self
-        return (
-            super().absolute()
-            if on_windows()
-            else self.__class__.__new__(
-                self.__class__, self._mount_points.get("/", ""), Path.cwd(), self
-            )
-        )  # type: ignore
-
-    def as_native(self) -> UPurePath:
-        if on_windows():
-            return self
-        path = self.absolute()
-        return UPurePath(
-            self._drive_mapping.get(path.drive, "/"),
-            path.relative_to(PureWindowsPath(path.anchor)),
-        )
-
-
-class UPath(UPurePath):
-    __slots__ = ()
+UFilePath = Union[str, UPath]
 
 
 class UPosixPath(UPath):
@@ -271,27 +197,49 @@ class UPosixPath(UPath):
     _flavour = _posix_flavour
 
     def __new__(cls, *args: FilePath, **kwargs: Any) -> UPosixPath:
-        self: UPosixPath = UPosixPath._from_parts(args)  # type: ignore
+        self: UPosixPath = UPosixPath._from_parts(args)
         self._new_winepfx()
         return self
 
-    def as_windows(self) -> UWindowsPath:
+    def _map_parts(self) -> tuple[str, UPosixPath]:
+        mnt = self.mount_point()
+        drive = self._mount_points.get(str(mnt), "")
+        path = self.expanduser().absolute()
+        path = path.relative_to(mnt) if drive else path
+        drive = drive or self._mount_points.get("/", "")
+        return drive, path  # type: ignore
+
+    def as_windows(self) -> UWinePath:
         drive, path = self._map_parts()  # type: ignore
-        return UWindowsPath(f"{drive}/{path}")
+        return UWinePath(f"{drive}/{path}")
 
 
 class UWindowsPath(UPath):
+    __slots__ = ()
+    _flavour = _windows_flavour
+
+    def __new__(cls, *args: FilePath, **kwargs: Any) -> UWindowsPath:
+        self: UWindowsPath = UWindowsPath._from_parts(args)
+        return self
+
+
+class UWinePath(UPath):
     __slots__ = ("_uwdrive", "_posix_path", "__dict__")
     __upath_api__: dict[str, Any] = {}
     _flavour = _wine_flavour
 
-    def __new__(cls, *args: FilePath, **kwargs: Any) -> UWindowsPath:
-        self: UWindowsPath = UWindowsPath._from_parts(args)  # type: ignore
+    def __new__(cls, *args: FilePath, **kwargs: Any) -> UWinePath:
+        self: UWinePath = UWinePath._from_parts(args)
         self._new_winepfx()
         self.__dict__["_flavour"] = _wine_flavour
         self._uwdrive, path = split_drive(self) if args else ("", args[0])
         self._posix_path = self.as_native()
         return self
+
+    def __getattr__(self, attr: str) -> Any:
+        if attr in self.__slots__:
+            return None
+        raise AttributeError(attr)
 
     def __getattribute__(self, attr: str) -> Any:
         cls = object.__getattribute__(self, "__class__")
@@ -309,8 +257,17 @@ class UWindowsPath(UPath):
         return _wrapped
 
     @classmethod
-    def home(cls) -> UWindowsPath:
+    def home(cls) -> UWinePath:
         return UPosixPath(Path.home()).as_windows()
+
+    def absolute(self) -> UWinePath:
+        return (
+            self
+            if self.is_absolute()
+            else self.__class__.__new__(
+                self.__class__, self._mount_points.get("/", ""), Path.cwd(), self
+            )
+        )
 
     def as_native(self) -> UPosixPath:
         path = self.absolute()
--- tests/test_path.py
+++ tests/test_path.py
@@ -1,8 +1,9 @@
 import subprocess
-from pathlib import Path
+from pathlib import Path, PurePath, PurePosixPath, PureWindowsPath
 
 import pytest
-from lyndows.path import UPurePosixPath, UPureWindowsPath
+from lyndows.path import UPath, UPosixPath, UWindowsPath, UWinePath
+from lyndows.util import on_windows
 
 
 def winepath(path: str, mode: str) -> str:
@@ -11,56 +12,114 @@ def winepath(path: str, mode: str) -> str:
     ).strip()
 
 
-class TestUPurePosixPath:
+class TestUPath:
+    def test_new(self):
+        if on_windows:
+            assert isinstance(UPath("/home/user/test"), UWindowsPath)
+        else:
+            assert isinstance(UPath("/home/user/test"), UPosixPath)
+            assert isinstance(UPath("c:/home/user/test"), UWinePath)
+
+
+class TestUWindowsPath:
+    def test_new(self):
+        if not on_windows():
+            with pytest.raises(NotImplementedError):
+                UPosixPath("/home/user/test")
+            return
+        path = UPosixPath("/home/user/test")
+        assert path.__class__.__mro__ == (
+            UWindowsPath,
+            UPath,
+            Path,
+            PureWindowsPath,
+            PurePath,
+            object,
+        )
+
+
+class TestUWinePath:
+    def test_new(self):
+        if on_windows():
+            with pytest.raises(NotImplementedError):
+                UPosixPath("/home/user/test")
+            return
+        path = UWinePath("/home/user/test")
+        assert path.__class__.__mro__ == (
+            UWinePath,
+            UPath,
+            Path,
+            PureWindowsPath,
+            PurePath,
+            object,
+        )
+
+
+class TestUPosixPath:
+    def test_new(self):
+        if on_windows():
+            with pytest.raises(NotImplementedError):
+                UPosixPath("/home/user/test")
+            return
+        path = UPosixPath("/home/user/test")
+        assert path.__class__.__mro__ == (
+            UWinePath,
+            UPath,
+            Path,
+            PurePosixPath,
+            PurePath,
+            object,
+        )
+
     def test_expanduser_with_tilde(self):
-        path = UPurePosixPath("~/case")
+        path = UPosixPath("~/case")
         expanded_path = path.expanduser()
-        expected_result = UPurePosixPath(Path.home() / "case")
+        expected_result = UPosixPath(Path.home() / "case")
         assert expanded_path == expected_result
 
     def test_expanduser_without_tilde(self):
-        path = UPurePosixPath("path/to/file")
+        path = UPosixPath("path/to/file")
         expanded_path = path.expanduser()
-        assert expanded_path == UPurePosixPath("path/to/file")
+        assert expanded_path == UPosixPath("path/to/file")
 
     def test_absolute(self):
         # Test when the path is already absolute
-        path = UPurePosixPath("/home/user/test")
+        path = UPosixPath("/home/user/test")
         assert path.absolute() == path
 
         # Test when the path is relative
-        path = UPurePosixPath("test")
-        expected_result = UPurePosixPath(Path.cwd() / path)
+        path = UPosixPath("test")
+        expected_result = UPosixPath(Path.cwd() / path)
         assert path.absolute() == expected_result
 
     def test_is_absolute(self):
         # Test when the path is absolute
-        path = UPurePosixPath("/home/user/test")
+        path = UPosixPath("/home/user/test")
         assert path.is_absolute()
 
         # Test when the path is relative
-        path = UPurePosixPath("test")
+        path = UPosixPath("test")
         assert not path.is_absolute()
 
     def test_joinpath(self):
         # Test joining a path with a string
-        path = UPurePosixPath("/home/user")
+        path = UPosixPath("/home/user")
         joined_path = path.joinpath("test")
         assert str(joined_path) == "/home/user/test"
 
         # Test joining a path with another path
-        path1 = UPurePosixPath("/home/user")
-        path2 = UPurePosixPath("test")
+        path1 = UPosixPath("/home/user")
+        path2 = UPosixPath("test")
         joined_path = path1.joinpath(path2)
         assert str(joined_path) == "/home/user/test"
 
     def test_is_mount(self):
         # Test when the path is a mount point
-        path = UPurePosixPath("/")
+        path = UPosixPath("/")
         assert path.is_mount()
 
         # Test when the path is not a mount point
-        path = UPurePosixPath("/home/user/test")
+        path = UPosixPath("/home/user/test")
         assert not path.is_mount()
 
     def test_mount_point(self):
@@ -71,7 +130,7 @@ class TestUPurePosixPath:
         # Test converting a POSIX path to a Windows path
         _str = "/home/user/test"
         _wstr = winepath(_str, "w")
-        path = UPurePosixPath(_str)
+        path = UPosixPath(_str)
         windows_path = path.as_windows()
-        assert UPureWindowsPath(_wstr) == windows_path
+        assert UWinePath(_wstr) == windows_path
         # assert str(windows_path) == _wstr
